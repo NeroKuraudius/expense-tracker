45277ff1dc9fd39f330c5083e692069d20139f89
Nero <karta0706566@gmail.com>
2023-04-02 21:36:33 +0800

    feat:add authenticator

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const { authenticator } = require('../middleware/auth')
 const home = require('./modules/home')
 const user = require('./modules/user')
 const expense = require('./modules/expense')
 
 router.use('/users', user)
-router.use('/', home)
-router.use('/expense', expense)
+router.use('/expense', authenticator, expense)
+router.use('/', authenticator, home)
 
 module.exports = router
