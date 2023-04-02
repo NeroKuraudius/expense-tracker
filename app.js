d4e76699fe712f614356fb9438c69096e24c2be6
Nero <karta0706566@gmail.com>
2023-04-02 22:07:07 +0800

    feat: add isAuthenticated to display different views

--- app.js
+++ app.js
@@ -23,7 +23,14 @@ app.use(session({
 }))
 
 usePassport(app)
+
 app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(router)
 
 app.listen(port, () => {
