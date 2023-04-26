c9e55b285be2fa9510a1a4d487e9cd3f86ac49de
NeroKuraudius <102511708+NeroKuraudius@users.noreply.github.com>
2023-04-26 13:28:57 +0800

    modify: eliminate parameter never use

--- routers/modules/user.js
+++ routers/modules/user.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
-const flash = require('connect-flash')
 const User = require('../../models/User')
 const bcrypt = require('bcryptjs')
 
