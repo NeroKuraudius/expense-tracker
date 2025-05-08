2b9d85df66ff1ae405f3fc61762c7a082eed7c70
Nero Hsu <nero.hsu@inspiredgt.local>
2025-05-08 17:05:33 +0800

    feat: add pagination-helper.js

---
+++ helpers/pagination-helper.js
@@ -0,0 +1,17 @@
+const getOffset = (limit = 10, page = 1) => {
+    return (page - 1) * limit
+}
+
+const getPagination = (limit = 10, page = 1, total = 50) =>{
+    const totalPage = Math.ceil(total/limit)
+    const pages = Array.from({ length: totalPage }, (_, index)=> index + 1)
+    
+    const currentPage = page < 1 ? 1 : page > totalPage ? totalPage : page
+    const prevPage = currentPage - 1 < 1 ? 1 : currentPage - 1
+    const nextPage = currentPage + 1 > totalPage ? totalPage : currentPage + 1
+
+    return { pages, totalPage, currentPage, prevPage, nextPage}
+}
+
+
+module.exports = { getOffset, getPagination }
