32cdb4be6b0aa21816b79feafd7e7123f9e4b73c
Nero Hsu <nero.hsu@inspiredgt.local>
2025-05-22 14:31:04 +0800

    feat: add ifGreaterThan in hbadlebars-helpers.js

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,8 +1,25 @@
 module.exports = {
   ifCond: function (a, b, options) {
+    // 如果有一個為空，則返回反向結果
     if (!a || !b) {
-      return options.inverse(this); // 如果有一個為空，則返回反向結果
+      return options.inverse(this); 
     }
+
     return a.toString() === b.toString() ? options.fn(this) : options.inverse(this);
+  },
+  ifGreaterThan: function (a, b, options) {
+    // 若未定義則返回反向結果
+    if (typeof a === 'undefined' || typeof b === 'undefined'){
+      return options.inverse(this)
+    }
+
+    const numA = Number(a)
+    const numB = Number(b)
+
+    if (isNaN(numA) || isNaN(numB)){
+      return options.inverse(this)
+    }
+
+    return numA > numB ? options.fn(this) : options.inverse(this)
   }
 }
