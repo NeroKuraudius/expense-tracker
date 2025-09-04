6cc303cd7920902c5ce516e0ffc16dcd679ed864
Nero Hsu <nero.hsu@inspiredgt.local>
2025-09-04 12:20:44 +0800

    chore: remove redundant notes

--- helpers/pagination-helpers.js
+++ helpers/pagination-helpers.js
@@ -3,12 +3,12 @@ const getOffset = (limit = 10, page = 1) => {
 }
 
 const getPagination = (limit = 10, page = 1, total = 50) =>{
-    const totalPage = Math.ceil(total/limit) //2 
+    const totalPage = Math.ceil(total/limit)
     const pages = Array.from({ length: totalPage }, (_, index)=> index + 1)
     
-    const currentPage = page < 1 ? 1 : page > totalPage ? totalPage : page // 1
-    const prevPage = currentPage - 1 < 1 ? 1 : currentPage - 1 // 1
-    const nextPage = currentPage + 1 > totalPage ? totalPage : currentPage + 1 // 2
+    const currentPage = page < 1 ? 1 : page > totalPage ? totalPage : page
+    const prevPage = currentPage - 1 < 1 ? 1 : currentPage - 1
+    const nextPage = currentPage + 1 > totalPage ? totalPage : currentPage + 1
 
     return { pages, totalPage, currentPage, prevPage, nextPage }
 }
