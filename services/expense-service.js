2413c1e399e011033f9e537cf4f7409ffce73476
Nero Hsu <nero.hsu@inspiredgt.local>
2025-09-04 12:08:33 +0800

    refactor: adjsut promise into async/await

--- services/expense-service.js
+++ services/expense-service.js
@@ -3,73 +3,96 @@ const Category = require('..//models/Category')
 
 const expenseService = {
     // 新增頁面
-    getExpense: (req,cb)=>{
-        return Category.find()
-            .lean()
-            .then(categories => { return cb(null, { categories }) })
-            .catch(err => cb(err.message))
+    getExpense: async(req,cb)=>{
+        try{
+            const categories = await Category.find().lean()
+            return cb(null, { categories })
+        }catch(err){
+            console.error('[Service] 新增支出頁面渲染失敗', err)
+            return cb(err.message)
+        }
     },
 
     // 修改頁面
-    editExpense: (req,cb)=>{
+    editExpense: async(req,cb)=>{
         const userId = req.user._id
         const _id = req.params.id
     
-        return Category.find()
-        .lean()
-        .then(categories => {
-            return Expense.findOne({ _id, userId })
+        try{
+            const categories = await Category.find().lean()
+            const expense = await Expense.findOne({ _id, userId })
                 .populate('categoryId')
                 .lean()
-                .then(item => { return cb(null, { item, categories }) })
-                .catch(err => cb(err.message))
-        })
-        .catch(err => console.log(err))
+
+            if (expense){
+                return cb(null,{ expense, categories })
+            }
+        }catch(err){
+            console.error('[Service] 支出修改頁面渲染失敗', err)
+            return cb(err.message)
+        }
     },
 
     // 修改支出
-    putExpense: (req,cb)=>{
+    putExpense: async(req,cb)=>{
         const userId = req.user._id
         const _id = req.params.id
         const { name, date, categoryId, cost } = req.body
 
-        return Expense.findOneAndUpdate({ _id, userId }, { name, date, categoryId, cost })
-            .then(() => { return cb(null, {}) })
-            .catch(err => cb(err.message))
+        try{
+            const updatedExpense = await Expense.findOneAndUpdate({ _id, userId }, { name, date, categoryId, cost })
+            if (updatedExpense){
+                return cb(null, {})
+            }else{
+                return cb(null, { errs: [{ errMsg: '支出修改失敗' }] })
+            }
+        }catch(err){
+            console.error('[Service] 支出修改失敗', err)
+            return cb(err.message)
+        }
     },
 
     // 刪除支出
-    deleteExpense: (req,cb)=>{
+    deleteExpense: async(req,cb)=>{
         const userId = req.user._id
         const _id = req.params.id
 
-        return Expense.findOne({ userId, _id })
-            .then(item => item.deleteOne({ _id })) // 舊版是 item.remove()
-            .then(() => { return cb(null, {}) })
-            .catch(error => cb(err.message))
+        try{
+            const deletedExpense = await Expense.findOneAndDelete({ userId, _id })
+        
+            if (deletedExpense) {
+                return cb(null, {})
+            } else {
+                return cb(null, { errs: [{ errMsg: '支出刪除失敗' }] })
+            }
+        }catch(err){
+            console.error('[Service] 支出刪除失敗', err)
+            return cb(err.message)
+        }
     },
 
     // 新增支出
-    postExpense: (req,cb)=>{
+    postExpense: async(req,cb)=>{
         const userId = req.user._id
         const { name, date, categoryId, cost } = req.body
 
-        if (!name.trim()){
-            return Category.find()
-                .lean()
-                .then(categories => { return cb(null, { name, date, cost, categoryId, categories }) })
-                .catch(err => cb(err.message))
-        }else if (!date || !categoryId || !cost) {
-            return Category.find()
-                .lean()
-                .then(categories => { return cb(null, { name, date, cost, categoryId, categories }) })
-                .catch(err => cb(err.message))
-        }
-
-        return Expense.create({ name, date, cost, categoryId, userId })
-            .then(() => { return  cb(null, {}) })
-            .catch(err => cb(err.message))
+        try{
+            if (!name.trim() || !date || !categoryId || !cost){
+                const categories = await Category.find().lean()
+                return cb(null, { name, date, cost, categoryId, categories })
             }
+
+            const newExpense = await Expense.create({ name, date, cost, categoryId, userId })
+            if (newExpense){
+                return cb(null, {})
+            }else{
+                return cb(null, { errs: [{ errMsg: '支出新增失敗' }]})
+            }
+        }catch(err){
+            console.error('[Service] 支出新增失敗', err)
+            return cb(err.message)
+        }
+    }
 }
 
 
